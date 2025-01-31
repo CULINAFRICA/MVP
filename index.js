732328573fbae0fa4089f141e064aa3da090484c
User <user@Users-MacBook-Pro.local>
2025-01-31 10:32:37 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,154 @@
+
+
+var randomNumbers = [];
+var africanDishes = [ "Alloco", "Brik", "Claclo", "Dibi", "Eguisi", "FouFou", "Garri", "Hawawshi", "Injera", "Jollof", "Kedjenou", "Lafidi", "Mafe" ];
+
+
+var africanDishesDescription = africanDishes.flatMap((dish, index) => [
+    { id: index, name: dish },
+    { id: index, name: dish + "-D" }
+]);
+
+
+var randomDishIndex = Math.floor(Math.random() * africanDishes.length); //
+
+// get random numbers to toggle between name of image or description from the arraylist
+var randomDescriptionAnswer = Math.floor(Math.random() * 2); //
+var randomDescriptionAnswerOption1 = Math.floor(Math.random() * 2); //
+var randomDescriptionAnswerOption2 = Math.floor(Math.random() * 2); //
+var randomDescriptionAnswerOption3 = Math.floor(Math.random() * 2); //
+
+console.log(randomDishIndex);
+console.log(africanDishes[randomDishIndex]);
+
+//gets answer using randomDish id from the africanDishsDescription array
+var dishDescriptionAnswer = africanDishesDescription.filter(obj => obj.id ===randomDishIndex);
+console.log(dishDescriptionAnswer[0].name);
+console.log(dishDescriptionAnswer[1].name);
+
+
+var dishDescriptionOptions = generateUniqueNumbers();
+// for (var i = 0; i < africanDishes.length; i++){
+//     var randomDish = Math.floor(Math.random()* 11);
+//     randomNumbers.push(randomNumber);
+// }
+console.log("dishDescriptionOptions "+dishDescriptionOptions)
+// console.log("dish description Options " +dishDescriptionOptions[0])
+// console.log("dish description Options " +dishDescriptionOptions[1])
+// console.log("dish description Options " +dishDescriptionOptions[2])
+// console.log("dish description Options " +dishDescriptionOptions[3])
+
+//set new question image from africanDishes array
+document.querySelector(".drop-piece-1").setAttribute("src", "./images/img/"+africanDishes[randomDishIndex]+"-P.png");
+
+// gets the answer to the picture above and puts in any of the 4 options
+document.querySelector(".drag-piece-"+dishDescriptionOptions[0]).setAttribute("src", "./images/img-d/"+(dishDescriptionAnswer[randomDescriptionAnswer].name)+".png");
+
+
+//generate 3 other pictures from dishDescription array that is different from the picture question
+var otherOptions = (generateUniqueDishes(randomDishIndex))
+console.log("otherOptions", otherOptions);
+var dishDescriptionOtherOptions1 = africanDishesDescription.filter(obj => obj.id === otherOptions[0]);
+var dishDescriptionOtherOptions2 = africanDishesDescription.filter(obj => obj.id === otherOptions[1]);
+var dishDescriptionOtherOptions3 = africanDishesDescription.filter(obj => obj.id === otherOptions[2]);
+document.querySelector(".drag-piece-"+dishDescriptionOptions[1]).setAttribute("src", "./images/img-d/"+(dishDescriptionOtherOptions1[randomDescriptionAnswerOption1].name)+".png");
+document.querySelector(".drag-piece-"+dishDescriptionOptions[2]).setAttribute("src", "./images/img-d/"+(dishDescriptionOtherOptions2[randomDescriptionAnswerOption2].name)+".png");
+document.querySelector(".drag-piece-"+dishDescriptionOptions[3]).setAttribute("src", "./images/img-d/"+(dishDescriptionOtherOptions3[randomDescriptionAnswerOption3].name)+".png");
+
+//function to generate the remining 3 options excluding the index of the answer to the question
+function generateUniqueDishes(excludeIndex) {
+    let availableIndices = africanDishes.map((_, index) => index).filter(index => index !== excludeIndex);
+    let result = [];
+    while (result.length < 3) {
+        let randomIndex = Math.floor(Math.random() * availableIndices.length);
+        result.push(availableIndices.splice(randomIndex, 1)[0]); // Remove and push
+    }
+    return result;
+}
+// console.log(generateUniqueDishes(randomDish));
+
+
+//generate 4 unique numbers to toggle the answer to any of the 4 options given
+function generateUniqueNumbers() {
+    let numbers = [1, 2, 3, 4]; // Possible numbers
+    let result = [];
+    
+    while (numbers.length > 0) {
+        let randomIndex = Math.floor(Math.random() * numbers.length);
+        result.push(numbers.splice(randomIndex, 1)[0]); // Remove and push
+    }
+    
+    return result;
+}
+
+// console.log(generateUniqueNumbers());
+
+document.getElementById("container").addEventListener("click", function(){
+    window.location.reload();
+})
+
+var options1 = document.querySelector(".drag-piece-"+dishDescriptionOptions[0]);
+console.log ("Option A "+ options1);
+var options2 = document.querySelector(".drag-piece-"+dishDescriptionOptions[1]);
+console.log ("Option B "+ options2);
+var options3 = document.querySelector(".drag-piece-"+dishDescriptionOptions[2]);
+console.log ("Option C "+ options3);
+var options4 = document.querySelector(".drag-piece-"+dishDescriptionOptions[3]);
+console.log ("Option D "+ options4);
+
+
+options1.addEventListener("drag", function(event){
+    console.log(event.target.id + " is being dragged.");
+});
+options2.addEventListener("drag", function(event){
+    console.log(event.target.id + " is being dragged.");
+});
+
+options3.addEventListener("drag", function(event){
+    console.log(event.target.id + " is being dragged.");
+});
+
+options4.addEventListener("drag", function(event){
+    console.log(event.target.id + " is being dragged.");
+});
+
+
+
+
+
+const answerDrop = document.querySelector(".drop-1");
+console.log(answerDrop);
+
+let beingDragged;
+answerDrop.addEventListener("dragover", function(event){
+    event.preventDefault();
+    console.log("You are dragging something over " + event.target.classList);
+});
+answerDrop.addEventListener("dragenter", function(event){
+    event.target.classList.add("highlight");
+    console.log("You entered " + event.target.classList);
+});
+
+answerDrop.addEventListener("dragleave", function(event){
+    event.target.classList.remove("highlight");
+    console.log("You left " + event.target.classList);
+});
+answerDrop.addEventListener("drop", function(event){
+    event.preventDefault();
+    event.target.append(event.target);
+    console.log("You dropped " + event.target.classList);
+    console.log("And dropped on " + answerDrop.classList);
+    if(event.target.classList.contains("drag-piece-"+dishDescriptionOptions[0])){
+        console.log("Correct Answer!");
+    } else {
+        console.log("Wrong Answer!");
+    }
+    event.target.classList.remove("highlight");
+});
+answerDrop.addEventListener("dragend", function(event){
+    event.target.classList.add("target");
+    setTimeout(()=> event.target.classList.remove("target"),100);
+    console.log("You are no longer dragging");
+});
+
+// answer.addEventListener("dragend",);
