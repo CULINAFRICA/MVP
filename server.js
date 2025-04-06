7b77054015bb7076f0a1da313de7d9b08ba7f834
User <user@Users-MacBook-Pro.local>
2025-04-05 22:42:21 -0500

    updated backend port

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ dotenv.config();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 
 const db = new pg.Client({
